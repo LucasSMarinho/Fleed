21ae8b291e0f325649ac7f4097bc2769361edaf9
LUCAS SOUZA MARINHO <45193262813@info.local>
2026-09-10 10:12:01 -0300

    adicionado componente Header

--- src/app/(tabs)/perfil/index.jsx
+++ src/app/(tabs)/perfil/index.jsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import Header from '../../../components/header/Header';
 import perfilStyles from './perfilStyles';
 
 export default function Perfil() {
   return (
+    <>
+    <Header tituloHeader="Login" corText="#fff"/>
     <View style={perfilStyles.container}>
       <Text>Login</Text>
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
