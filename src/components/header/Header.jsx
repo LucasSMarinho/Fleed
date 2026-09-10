c120fd25d5be66fd69ba4ffcca591a2e92b197a0
LUCAS SOUZA MARINHO <45193262813@info.local>
2026-09-10 10:22:21 -0300

    Subindo alterações no Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import { Text, View } from 'react-native';
+import { Image } from 'react-native';
+import backIcon from '../../../assets/backIcon.png';
+import headerStyles from './headerStyles';
+import { useFonts, Oswald_600SemiBold} from '@expo-google-fonts/oswald';
+
+export default function Header(props) {
+
+    let [fontsLoaded] = useFonts({
+        Oswald_600SemiBold,
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <>
+            <View style={headerStyles.container}>
+                <touchebleOpacity>
+                <Image source={backIcon} style={{height: 13, width: 13}}/>
+                </touchebleOpacity>
+                <Text style={[headerStyles.text, {color: `${props.corText}`}]} > {props.tituloHeader} </Text>
+            </View>
+        </>
+    );
+}
