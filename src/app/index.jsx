5aeaad61ff153d14426d1933ea7834f75c863114
LUCAS SOUZA MARINHO <45193262813@info.local>
2026-09-10 08:55:26 -0300

    Subindo projeto com tabs

---
+++ src/app/index.jsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+import { useRouter } from 'expo-router';
+
+export default function App() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Text>Essa é a tela inicial</Text>
+      <TouchableOpacity onPress={() => {router.push('/(tabs)')}}>
+        <Text>Clique aqui</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
