02a61f9c5fe262e58463f8d12f901b023903ee87
LORENZO MANGILE FARIA <53101823811@info.local>
2026-09-10 16:32:17 -0300

    Alteracoes na novaPublicacao

--- src/app/(tabs)/novaPublicacao/novaPublicacaoStyles.js
+++ src/app/(tabs)/novaPublicacao/novaPublicacaoStyles.js
@@ -1,3 +1,4 @@
+
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
@@ -14,14 +15,6 @@ const styles = StyleSheet.create({
         paddingBottom: 20,
     },
 
-    tituloPagina: {
-        fontSize: 20,
-        fontWeight: "700",
-        color: "#588EB2",
-        textAlign: "center",
-        marginBottom: 18,
-    },
-
     header: {
         width: "100%",
         flexDirection: "row",
@@ -35,12 +28,21 @@ const styles = StyleSheet.create({
         position: "absolute",
         left: 0,
         padding: 5,
+        
     },
 
     backIcon: {
         width: 28,
         height: 28,
         resizeMode: "contain",
+    },
+
+    tituloPagina: {
+        fontSize: 20,
+        fontWeight: "700",
+        color: "#588EB2",
+        textAlign: "center",
+        marginBottom: 18,
     },
 
     tituloCampo: {
@@ -59,9 +61,55 @@ const styles = StyleSheet.create({
         borderRadius: 9,
         paddingHorizontal: 9,
         paddingTop: 10,
+        paddingBottom: 10,
         color: "#868686",
         textAlignVertical: "top",
-        paddingVertical: 5,
+    },
+
+    opcoes: {
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 25,
+        marginBottom: 30,
+    },
+
+    opcao: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginRight: 25,
+    },
+
+    textoOpcao: {
+        fontSize: 16,
+        color: "#588EB2",
+        marginLeft: 5,
+        fontWeight: "600",
+        
+    },
+
+    publicar: {
+        width: "100%",
+        height: 54,
+        backgroundColor: "#C3375B",
+        borderRadius: 8,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    textoPublicar: {
+        fontSize: 26,
+        fontWeight: "700",
+        color: "#fff",
+    },
+
+    fundo: {
+        position: "absolute",
+        bottom: -30,
+        left: 0,
+        width: "100%",
+        height: 100,
+       
     },
 });
 
