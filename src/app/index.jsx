af8ce090da5feb7ac0a1eb0cef2046e669f371a4
LAURA KAUANY DOS SANTOS ANACLETO <54000450816@info.local>
2026-09-10 10:30:28 -0300

    Criada Tela de Index

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,27 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-web';
-import { useRouter } from 'expo-router';
+import { StatusBar } from 'expo-status-bar'; 
+import { StyleSheet, Text, View, Image } from 'react-native'; 
+import { TouchableOpacity } from 'react-native'; 
+import { useRouter } from 'expo-router'; 
+import logo from "../../assets/logo.png";
+ 
+export default function App() { 
+  const router = useRouter(); 
+ 
+  return ( 
+    <View style={styles.container}> 
 
-export default function App() {
-  const router = useRouter();
+      <Image
+        source={logo}
+        style={styles.logo}
+      />
 
-  return (
-    <View style={styles.container}>
-      <Text>Essa é a tela inicial</Text>
-      <TouchableOpacity onPress={() => {router.push('/perfil')}}>
-        <Text>Clique aqui</Text>
+      <Text style={styles.title}>Fleed</Text>
+
+      <Text style={styles.subtitle}>
+        Conecte-se com Pessoas e{'\n'}
+        compartilhe momentos.
+      </Text>
+
+      <TouchableOpacity 
+        style={styles.entrar}
+        onPress={() => {router.push('/perfil')}} 
+      > 
+        <Text style={styles.buttonText}>Entrar</Text> 
       </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+      <TouchableOpacity 
+        style={styles.criar}
+        onPress={() => {router.push('/perfil')}} 
+      > 
+        <Text style={styles.buttonText}>Criar conta</Text> 
+      </TouchableOpacity>
+
+      <Image
+        source={require('../../assets/fundo.png')}
+        style={styles.fundo}
+      />
+
+      <StatusBar style="auto" /> 
+    </View> 
+  ); 
+} 
+ 
+const styles = StyleSheet.create({ 
+  container: { 
+    flex: 1, 
+    backgroundColor: '#fff', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 15,
+  },
+
+  title: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    color: '#BC3154',
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: '#6D95AA',
+    textAlign: 'center',
+    lineHeight: 24,
+    marginTop: 5,
+    marginBottom: 55,
+  },
+
+  entrar: {
+    width: '85%',
+    height: 50,
+    backgroundColor: '#BF3156',
+    borderRadius: 7,
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 16,
   },
+
+  criar:{
+    width: "85%", 
+    height: 50, 
+    backgroundColor: "#DC97A5",
+    borderRadius: 7,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 16,
+  },
+  buttonText:{
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  fundo: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: 65,
+  },
+
+  
 });
