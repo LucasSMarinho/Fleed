8fb94843e22705fa7856ed705c0bb12dd5ca6c21
LUCAS SOUZA MARINHO <45193262813@info.local>
2026-09-10 09:11:04 -0300

    Pasta de components atualizada

--- src/app/(tabs)/perfil/perfilStyles.js
+++ src/app/(tabs)/perfil/perfilStyles.js
@@ -1,9 +1,9 @@
 import { StyleSheet } from 'react-native';
 
-const perfilStyles = StyleSheet.create({
+const perfilStyles   = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#588EB2',
     alignItems: 'center',
     justifyContent: 'center',
   },
